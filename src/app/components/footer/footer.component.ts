771d683fc4e5e1bd67e1758e75d5cb72f0567fa5
lhofman <hofmanlennert@gmail.com>
2020-05-23 15:58:21 +0200

    Creates the footer component

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -2,9 +2,23 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  templateUrl: './footer.component.html'
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  currentYear = new Date().getFullYear();
+  aboutMe = 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, \
+  when an unknown printer took a galley of type and scrambled it to make a type specimen book.';
+  links = [
+    {
+      'value': 'Pinterest',
+      'url': 'https://google.be'
+    },
+    {
+      'value': 'Instagram',
+      'url': 'https://google.be'
+    }
+  ];
 
   constructor() { }
 
